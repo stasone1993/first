c3474a2dd17a286dc8febebcbe95fe9880691d2e
meduza <meduza@.com>
2016-10-26 20:19:22 +0300

    Start page of site and nb to gitignore list

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Yo-ho-ho</h1>
+        <image src="http://www.extreme-factory.ru/wp-content/uploads/2011/12/Santa-Klausyi.jpg" style="width:100%"/>
+    </body>
+</html>
